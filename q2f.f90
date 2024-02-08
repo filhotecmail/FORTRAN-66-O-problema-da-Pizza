c5e47bf6167a0a4f8fbf82a9c8cee5fafe946eb2
Carlos Alberto Dias da Silva Filho <filhotecmail@gmail.com>
2024-02-08 05:12:44 +0000

    Criação da estrutura do programa e do arquivo de cabeçalho

---
+++ q2f.f90
@@ -0,0 +1,31 @@
+! PROGRAMA DIVISAOCONTA
+! ANALISTA: CARLOS DIAS
+! REVISION: 2024-02-08
+! OBJETIVO: SOLUÇÃO DO PROBLEMA MATEMÁTICO DE FRAÇÕES E EQUAÇÕES DO CURSO DE 
+! MATEMATICA APLICADA A CONCURSOS E VESTIBULARES
+!
+! MODULO: RACIOCÍNIO LÓGICO
+!
+! DESCRIÇÃO: O  programa visa calcular a divisão do valor de uma pizza do problema da pizza no enunciado, 
+! entre 5 amigos, e quanto cada um tem que pagar, considerando as infrações consumidas por 
+! cada um e o valor total da pizza com desconto
+!
+! VARIÁVEIS:
+!   A - Fração da pizza consumida por Andre
+!   P - Fração da pizza consumida por Paula
+!   F - Fração da pizza consumida por Fernando
+!   M - Fração da pizza consumida por Maria
+!   PE - Fração da pizza consumida por Pedro
+!   TOT - Valor total da pizza
+!   DESC - Valor do desconto aplicado no restaurante
+
+PROGRAM DIVISAOCONTA
+
+REAL A,P,F,M PE, TOT, DESC
+
+A = 1.0 / 5,0
+P = 1.0 / 3.0
+F = 1.0 / 8.0
+M = 1.0 / 6.0
+
+END PROGRAM DIVISAOCONTA
